da0a659a7d5446423a3eafdda0a3aca0a26e958f
rishgupta2301 <92207582+rishgupta2301@users.noreply.github.com>
2022-10-10 18:27:19 +0530

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1LWa9CSJmfBwT8S4fDuY4xgO' },
+          { shipping_rate: 'shr_1LWZRUSJmfBwT8S4oqY70P8n' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
 
           return {
             price_data: { 
-              currency: "usd",
+              currency: "inr",
               product_data: { 
                 name: item.name,
                 images: [newImage],
